c8f8de083c9eeae59d3a1f3a17a42c6aad0869a5
Daniel Kench <djk5788@rit.edu>
2022-05-23 18:14:21 -0400

    Created Country api services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Country} from "./country";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'higher-lower';
+  countries: Country[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
 }
