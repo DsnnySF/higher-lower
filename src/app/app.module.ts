d1ab83d09e7b538a01a331f7de5af3aac5a10cdc
Daniel Kench <djk5788@rit.edu>
2022-05-30 14:22:02 -0400

    The homepage background shows all ten flags for long enough (still shows brazil for too long). Start button is now visible and has a clean and cool style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
   ],
   imports: [
     BrowserModule
