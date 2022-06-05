f5393b7a2dfc5cc058be23774154e28ffcf5dcdb
Daniel Kench <djk5788@rit.edu>
2022-06-05 17:01:30 -0400

    Fixed gamemodes drop down menu and have an idea commented for how to do the difficulty part but it didn't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -12,8 +11,7 @@ import {MatSelectModule} from "@angular/material/select";
   ],
   imports: [
     BrowserModule,
-    MatFormFieldModule,
-    MatSelectModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/country.ts
+++ src/app/country.ts
@@ -5,4 +5,5 @@ export interface Country {
   pop: string;
   ratio: number;
   recognizedByUN: boolean;
+  popid: number;
 }
